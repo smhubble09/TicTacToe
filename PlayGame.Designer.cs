185b354ff802a0ad22b396ee70a8f1d6cc1922e0
shane.hubble <smhubble09@gmail.com>
2022-04-21 09:14:25 -0600

    Add project files.

---
+++ PlayGame.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace TicTacToe {
+    partial class PlayGame {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AA = new System.Windows.Forms.Button();
+            this.AB = new System.Windows.Forms.Button();
+            this.AC = new System.Windows.Forms.Button();
+            this.BA = new System.Windows.Forms.Button();
+            this.BB = new System.Windows.Forms.Button();
+            this.BC = new System.Windows.Forms.Button();
+            this.CA = new System.Windows.Forms.Button();
+            this.CB = new System.Windows.Forms.Button();
+            this.CC = new System.Windows.Forms.Button();
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.TurnLabel = new System.Windows.Forms.Label();
+            this.XWinsLabel = new System.Windows.Forms.Label();
+            this.OWinsLabel = new System.Windows.Forms.Label();
+            this.DrawsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(159, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(4, 380);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(321, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(4, 380);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(2, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(480, 4);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(2, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(480, 4);
+            this.label4.TabIndex = 3;
+            // 
+            // AA
+            // 
+            this.AA.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AA.Location = new System.Drawing.Point(7, 37);
+            this.AA.Name = "AA";
+            this.AA.Size = new System.Drawing.Size(146, 119);
+            this.AA.TabIndex = 4;
+            this.AA.TabStop = false;
+            this.AA.UseCompatibleTextRendering = true;
+            this.AA.UseVisualStyleBackColor = true;
+            this.AA.Click += new System.EventHandler(this.SquareButton_Click);
+            // 
+            // AB
+            // 
+            this.AB.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AB.Location = new System.Drawing.Point(169, 37);
+            this.AB.Name = "AB";
+            this.AB.Size = new System.Drawing.Size(146, 119);
+            this.AB.TabIndex = 5;
+            this.AB.TabStop = false;
+            this.AB.UseCompatibleTextRendering = true;
+            this.AB.UseVisualStyleBackColor = true;
+            this.AB.Click += new System.EventHandler(this.SquareButton_Click);
+            // 
+            // AC
+            // 
+            this.AC.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AC.Location = new System.Drawing.Point(331, 37);
+            this.AC.Name = "AC";
+            this.AC.Size = new System.Drawing.Size(146, 119);
+            this.AC.TabIndex = 6;
+            this.AC.TabStop = false;
+            this.AC.UseCompatibleTextRendering = true;
+            this.AC.UseVisualStyleBackColor = true;
+            this.AC.Click += new System.EventHandler(this.SquareButton_Click);
+            // 
+            // BA
+            // 
+            this.BA.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BA.Location = new System.Drawing.Point(7, 166);
+            this.BA.Name = "BA";
+            this.BA.Size = new System.Drawing.Size(146, 119);
+            this.BA.TabIndex = 7;
+            this.BA.TabStop = false;
+            this.BA.UseCompatibleTextRendering = true;
+            this.BA.UseVisualStyleBackColor = true;
+            this.BA.Click += new System.EventHandler(this.SquareButton_Click);
+            // 
+            // BB
+            // 
+            this.BB.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BB.Location = new System.Drawing.Point(169, 166);
+            this.BB.Name = "BB";
+            this.BB.Size = new System.Drawing.Size(146, 119);
+            this.BB.TabIndex = 8;
+            this.BB.TabStop = false;
+            this.BB.UseCompatibleTextRendering = true;
+            this.BB.UseVisualStyleBackColor = true;
+            this.BB.Click += new System.EventHandler(this.SquareButton_Click);
+            // 
+            // BC
+            // 
+            this.BC.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BC.Location = new System.Drawing.Point(331, 166);
+            this.BC.Name = "BC";
+            this.BC.Size = new System.Drawing.Size(146, 119);
+            this.BC.TabIndex = 9;
+            this.BC.TabStop = false;
+            this.BC.UseCompatibleTextRendering = true;
+            this.BC.UseVisualStyleBackColor = true;
+            this.BC.Click += new System.EventHandler(this.SquareButton_Click);
+            // 
+            // CA
+            // 
+            this.CA.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CA.Location = new System.Drawing.Point(7, 295);
+            this.CA.Name = "CA";
+            this.CA.Size = new System.Drawing.Size(146, 119);
+            this.CA.TabIndex = 10;
+            this.CA.TabStop = false;
+            this.CA.UseCompatibleTextRendering = true;
+            this.CA.UseVisualStyleBackColor = true;
+            this.CA.Click += new System.EventHandler(this.SquareButton_Click);
+            // 
+            // CB
+            // 
+            this.CB.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CB.Location = new System.Drawing.Point(169, 295);
+            this.CB.Name = "CB";
+            this.CB.Size = new System.Drawing.Size(146, 119);
+            this.CB.TabIndex = 11;
+            this.CB.TabStop = false;
+            this.CB.UseCompatibleTextRendering = true;
+            this.CB.UseVisualStyleBackColor = true;
+            this.CB.Click += new System.EventHandler(this.SquareButton_Click);
+            // 
+            // CC
+            // 
+            this.CC.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CC.Location = new System.Drawing.Point(331, 295);
+            this.CC.Name = "CC";
+            this.CC.Size = new System.Drawing.Size(146, 119);
+            this.CC.TabIndex = 12;
+            this.CC.TabStop = false;
+            this.CC.UseCompatibleTextRendering = true;
+            this.CC.UseVisualStyleBackColor = true;
+            this.CC.Click += new System.EventHandler(this.SquareButton_Click);
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.Location = new System.Drawing.Point(169, 8);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(308, 23);
+            this.NewGameButton.TabIndex = 13;
+            this.NewGameButton.Text = "New Game";
+            this.NewGameButton.UseVisualStyleBackColor = true;
+            this.NewGameButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // TurnLabel
+            // 
+            this.TurnLabel.AutoSize = true;
+            this.TurnLabel.Location = new System.Drawing.Point(7, 9);
+            this.TurnLabel.Name = "TurnLabel";
+            this.TurnLabel.Size = new System.Drawing.Size(0, 15);
+            this.TurnLabel.TabIndex = 14;
+            // 
+            // XWinsLabel
+            // 
+            this.XWinsLabel.AutoSize = true;
+            this.XWinsLabel.Location = new System.Drawing.Point(7, 426);
+            this.XWinsLabel.Name = "XWinsLabel";
+            this.XWinsLabel.Size = new System.Drawing.Size(46, 15);
+            this.XWinsLabel.TabIndex = 15;
+            this.XWinsLabel.Text = "X Wins:";
+            // 
+            // OWinsLabel
+            // 
+            this.OWinsLabel.AutoSize = true;
+            this.OWinsLabel.Location = new System.Drawing.Point(169, 426);
+            this.OWinsLabel.Name = "OWinsLabel";
+            this.OWinsLabel.Size = new System.Drawing.Size(48, 15);
+            this.OWinsLabel.TabIndex = 16;
+            this.OWinsLabel.Text = "O Wins:";
+            // 
+            // DrawsLabel
+            // 
+            this.DrawsLabel.AutoSize = true;
+            this.DrawsLabel.Location = new System.Drawing.Point(331, 426);
+            this.DrawsLabel.Name = "DrawsLabel";
+            this.DrawsLabel.Size = new System.Drawing.Size(42, 15);
+            this.DrawsLabel.TabIndex = 17;
+            this.DrawsLabel.Text = "Draws:";
+            // 
+            // PlayGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 453);
+            this.Controls.Add(this.DrawsLabel);
+            this.Controls.Add(this.OWinsLabel);
+            this.Controls.Add(this.XWinsLabel);
+            this.Controls.Add(this.TurnLabel);
+            this.Controls.Add(this.NewGameButton);
+            this.Controls.Add(this.CC);
+            this.Controls.Add(this.CB);
+            this.Controls.Add(this.CA);
+            this.Controls.Add(this.BC);
+            this.Controls.Add(this.BB);
+            this.Controls.Add(this.BA);
+            this.Controls.Add(this.AC);
+            this.Controls.Add(this.AB);
+            this.Controls.Add(this.AA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "PlayGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button AA;
+        private Button AB;
+        private Button AC;
+        private Button BA;
+        private Button BB;
+        private Button BC;
+        private Button CA;
+        private Button CB;
+        private Button CC;
+        private Button NewGameButton;
+        private Label TurnLabel;
+        private Label XWinsLabel;
+        private Label OWinsLabel;
+        private Label DrawsLabel;
+    }
+}
---
+++ PlayGame.cs
@@ -0,0 +1,120 @@
+﻿namespace TicTacToe {
+    public partial class PlayGame : Form {
+        string value = "X";
+        int turnCount = 0;
+        int xWins = 0;
+        int oWins = 0;
+        int draws = 0;
+
+        public PlayGame() {
+            InitializeComponent();
+            DrawsLabel.Text = "Draws: " + draws;
+            XWinsLabel.Text = "X Wins: " + xWins;
+            OWinsLabel.Text = "O Wins: " + oWins;
+            TurnLabel.Text = "It's X's turn";
+        }
+
+        private void SquareButton_Click(object sender, EventArgs e) {
+            Button button = sender as Button;
+            if (value == "X") {
+                button.Text = value;
+                button.Enabled = false;
+            }
+            else {
+                button.Text = value;
+                button.Enabled = false;
+            }
+            turnCount++;
+            checkWinner();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e) {
+            AA.Text = "";
+            AB.Text = "";
+            AC.Text = "";
+            BA.Text = "";
+            BB.Text = "";
+            BC.Text = "";
+            CA.Text = "";
+            CB.Text = "";
+            CC.Text = "";
+            AA.Enabled = true;
+            AB.Enabled = true;
+            AC.Enabled = true;
+            BA.Enabled = true;
+            BB.Enabled = true;
+            BC.Enabled = true;
+            CA.Enabled = true;
+            CB.Enabled = true;
+            CC.Enabled = true;
+            turnCount = 0;
+            value = "X";
+            TurnLabel.Text = "It's X's turn";
+        }
+
+        private void checkWinner() {
+            bool weHaveWinner = false;
+
+            //horizontal win
+            if (AA.Text == AB.Text && AB.Text == AC.Text && !AB.Enabled)
+                weHaveWinner = true;
+            else if (BA.Text == BB.Text && BB.Text == BC.Text && !BB.Enabled)
+                weHaveWinner = true;
+            else if (CA.Text == CB.Text && CB.Text == CC.Text && !CB.Enabled)
+                weHaveWinner = true;
+
+            //vertical win
+            else if (AA.Text == BA.Text && BA.Text == CA.Text && !BA.Enabled)
+                weHaveWinner = true;
+            else if (AB.Text == BB.Text && BB.Text == CB.Text && !BB.Enabled)
+                weHaveWinner = true;
+            else if (AC.Text == BC.Text && BC.Text == CC.Text && !BC.Enabled)
+                weHaveWinner = true;
+
+            //diagonal win
+            else if (AA.Text == BB.Text && BB.Text == CC.Text && !BB.Enabled)
+                weHaveWinner = true;
+            else if (AC.Text == BB.Text && BB.Text == CA.Text && !BB.Enabled)
+                weHaveWinner = true;
+
+            if (weHaveWinner) {
+                string winner = value;
+                MessageBox.Show(winner + " Wins!", "Good Game");
+                if (value == "X") {
+                    xWins++;
+                    XWinsLabel.Text = "X Wins: " + xWins;
+                }
+                else {
+                    oWins++;
+                    OWinsLabel.Text = "O Wins: " + oWins;
+                }
+
+                AA.Enabled = false;
+                AB.Enabled = false;
+                AC.Enabled = false;
+                BA.Enabled = false;
+                BB.Enabled = false;
+                BC.Enabled = false;
+                CA.Enabled = false;
+                CB.Enabled = false;
+                CC.Enabled = false;
+            }
+            else {
+                if (turnCount == 9) {
+                    MessageBox.Show("Draw", "No Winner");
+                    draws++;
+                    DrawsLabel.Text = "Draws: " + draws;
+                }
+                    
+                if (value == "X") {
+                    value = "O";
+                    TurnLabel.Text = "It's " + value + "'s turn";
+                }  
+                else {
+                    value = "X";
+                    TurnLabel.Text = "It's " + value + "'s turn";
+                }    
+            }
+        }
+    }
+}
